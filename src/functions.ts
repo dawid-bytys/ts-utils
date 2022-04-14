eda475d2b5709809a9ec42f2182c2c544ffb3d9d
salvia-dev <salviadev@protonmail.com>
2022-04-14 19:49:56 +0200

    feat(functions): added isObjectEmpty type guard

--- src/functions.ts
+++ src/functions.ts
@@ -1,11 +1,20 @@
 import { PRIMITIVES } from './constants';
-import type { Primitive } from './types';
+import type { Primitive, AnyObject, EmptyObject } from './types';
 
 /**
  * Determine if the value is a primitive
  */
 const isPrimitive = (value: unknown): value is Primitive => {
   return PRIMITIVES.some(primitive => typeof value === primitive);
+};
+
+/**
+ * Determine if the object is empty
+ */
+const isObjectEmpty = <T extends AnyObject>(
+  object: T,
+): object is EmptyObject => {
+  return Object.keys(object).length === 0;
 };
 
 /**
