595a985cc54a1bbe301dbc5f0d0e9db3a2a4c2a4
salvia-dev <salviadev@protonmail.com>
2022-04-19 22:14:35 +0200

    feat(functions): added capitalizeFirst function

--- src/functions.ts
+++ src/functions.ts
@@ -4,8 +4,8 @@ import type { Primitive, AnyObject, EmptyObject } from './types';
 /**
  * Determine if a value is a primitive
  */
-const isPrimitive = (value: unknown): value is Primitive => {
-  return PRIMITIVES.includes(typeof value);
+const isPrimitive = (val: unknown): val is Primitive => {
+  return PRIMITIVES.includes(typeof val);
 };
 
 /**
@@ -20,11 +20,18 @@ const isObjectEmpty = <T extends AnyObject>(
 /**
  * Create a unique array
  */
-const uniqueArray = <T>(array: T[]) => [...new Set(array)];
+const uniqueArray = <T>(arr: T[]) => [...new Set(arr)];
 
 /**
  * Determine if a value occurs in an array more than once
  */
-const isDuplicate = <T>(array: T[], value: T) => {
-  return array.indexOf(value) !== array.lastIndexOf(value);
+const isDuplicate = <T>(arr: T[], val: T) => {
+  return arr.indexOf(val) !== arr.lastIndexOf(val);
+};
+
+/**
+ * Capitalize a first letter of a string
+ */
+const capitalizeFirst = <T extends string>(str: T): Capitalize<T> => {
+  return (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<T>;
 };
