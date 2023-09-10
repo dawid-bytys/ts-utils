dd6fb9a38774515db6eb4421a17d3e9712c738af
dawid-bytys <dawid.bytys@proton.me>
2023-09-11 00:08:24 +0200

    refactor: syntax

--- src/functions.ts
+++ src/functions.ts
@@ -1,37 +1,30 @@
 import { PRIMITIVES } from './constants';
-import type { Primitive, AnyObject, EmptyObject } from './types';
+import type { Primitive, AnyObject } from './types';
 
 /**
  * Determine if a value is a primitive
  */
-const isPrimitive = (val: unknown): val is Primitive => {
-  return PRIMITIVES.includes(typeof val);
-};
-
-/**
- * Determine if an object is empty
- */
-const isObjectEmpty = <T extends AnyObject>(
-  object: T,
-): object is EmptyObject => {
-  return Object.keys(object).length === 0;
-};
+function isPrimitive(value: unknown): value is Primitive {
+  return PRIMITIVES.includes(typeof value);
+}
 
 /**
  * Create a unique array
  */
-const uniqueArray = <T>(arr: T[]) => [...new Set(arr)];
+function uniqueArray<T>(arr: T[]) {
+  return [...new Set(arr)];
+}
 
 /**
  * Determine if a value occurs in an array more than once
  */
-const isDuplicate = <T>(arr: T[], val: T) => {
-  return arr.indexOf(val) !== arr.lastIndexOf(val);
-};
+function isDuplicate<T>(arr: T[], value: T) {
+  return arr.indexOf(value) !== arr.lastIndexOf(value);
+}
 
 /**
  * Capitalize a first letter of a string
  */
-const capitalizeFirst = <T extends string>(str: T): Capitalize<T> => {
+function capitalizeFirst<T extends string>(str: T): Capitalize<T> {
   return (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<T>;
-};
+}
